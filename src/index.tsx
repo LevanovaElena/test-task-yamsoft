b4726f8abf9652b98893acdbc3092058d8e6fe6b
ellevanova <ellevanova@gmail.com>
2023-08-23 18:53:00 +0400

    add basename

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.css";
 import App from "./modules/app/App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
@@ -11,8 +11,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store()}>
-    <BrowserRouter basename={"/test-task-yamsoft/"}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
 );
