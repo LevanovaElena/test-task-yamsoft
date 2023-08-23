ba9e608e9a4c98794f55eae6671c13b9fb2a011e
ellevanova <ellevanova@gmail.com>
2023-08-23 23:02:21 +0400

    fixed errors

--- src/modules/navigation/navigation.component.tsx
+++ src/modules/navigation/navigation.component.tsx
@@ -12,7 +12,13 @@ export const NavigationComponent = (): React.JSX.Element => {
   return (
     <nav className="w-full fixed  top-0 left-0 right-0 z-50 flex items-center justify-between  h-[50px] bg-white   shadow-xl  px-1 xl:px-20 ">
       <div className="flex  justify-between text-gray-700  mx-5 xl:mx-20 w-full">
-        <h3 className="font-bold align-middle">Magazine</h3>
+        <Link to={"/"} className="ms-5">
+          <h3 className="font-bold align-middle  text-xl">
+            <span className="text-red-500">B</span>
+            ou<span className="text-red-500">T</span>i
+            <span className="text-red-500">Q</span>ue
+          </h3>
+        </Link>
         <div className="flex w-50 justify-end">
           <Link to={"/"} className="ms-5">
             <img src={IconHome} width={24} height={24} alt="Home" />
@@ -36,7 +42,7 @@ export const NavigationComponent = (): React.JSX.Element => {
               className=""
             />
             {cart && cart.products.length > 0 && (
-              <div className="rounded-full bg-red-700 text-white font-bold text-sm absolute bottom-[-10px] right-[-10px] w-[20px] h-[20px] text-center">
+              <div className="rounded-full bg-red-500 text-white font-bold text-sm absolute bottom-[-7px] right-[-10px] w-[20px] h-[20px] text-center">
                 {cart.products.reduce(
                   (sum, current) => sum + current.quantity,
                   0,
